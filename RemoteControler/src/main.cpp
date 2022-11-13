ccacc061901b42245d8e76baecca2eaed2bbe02a
Diodak92 <tomasz.kowalski92@icloud.com>
2022-11-14 00:11:42 +0100

    Added class for remote controller

--- RemoteControler/src/main.cpp
+++ RemoteControler/src/main.cpp
@@ -2,10 +2,78 @@
 #include <ArduinoJson.h>
 #include <LoRa.h>
 
-void setup() {
-  // put your setup code here, to run once:
+class RemoteController
+{
+private:
+  int switch_pin0, switch_pin1, switch_pin2;
+
+public:
+  bool switch_state_0, switch_state_1, switch_state_2;
+  RemoteController(int sw_pin0, int sw_pin1, int sw_pin2)
+  {
+    switch_pin0 = sw_pin0;
+    switch_pin1 = sw_pin1;
+    switch_pin2 = sw_pin2;
+  }
+
+  void set_switch_mode()
+  {
+    pinMode(switch_pin0, INPUT_PULLUP);
+    pinMode(switch_pin1, INPUT_PULLUP);
+    pinMode(switch_pin2, INPUT_PULLUP);
+  }
+
+  int read_swich_state()
+  {
+    // read state of switches (NC logic)
+    switch_state_0 = digitalRead(switch_pin0);
+    switch_state_1 = digitalRead(switch_pin1);
+    switch_state_2 = digitalRead(switch_pin2);
+    // code state 0 if both switches are shorted
+    if ((switch_state_0 & switch_state_2) == true)
+    {
+      return 0;
+    }
+    // code state 1 if switch0 is shorted and switch1 is open
+    else if (((switch_state_0 == true) & (~switch_state_2 == false)) == true)
+    {
+      return 1;
+    }
+    // code state 2 if both switches are open
+    else if ((switch_state_0 | switch_state_2) == false)
+    {
+      return 2;
+    }
+    // return error for uknown states
+    else
+    {
+      return -1;
+    }
+  }
+};
+
+// switches objects
+RemoteController heightSwitch(0, 1, 2);
+RemoteController massSwitch(3, 4, 5);
+
+void setup()
+{
+  // start serial comunication
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Setup started!");
+  heightSwitch.set_switch_mode();
+  massSwitch.set_switch_mode();
+  Serial.println("Setup successfully completedd!");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+ 
+  Serial.println("Height switch state: ");
+  Serial.print(heightSwitch.read_swich_state());
+  Serial.println("Mass switch state: ");
+  Serial.print(massSwitch.read_swich_state());
+  delay(2000);
 }
