582f89ebe570854929b642fb515bf9c5449efc8f
Kowalski <tomaszmarcin.kowalski@refinitiv.com>
2022-11-15 17:55:29 +0100

    Changed ADC read resolution

--- RemoteControler/src/main.cpp
+++ RemoteControler/src/main.cpp
@@ -5,14 +5,14 @@
 #define ADC_BATTERY A0      // baterry pin
 #define GREEN_LED_BATTERY 6 // baterry LED
 
-class RemoteController
+class Switch
 {
 private:
   int switch_pin0, switch_pin1, switch_pin2;
 
 public:
   bool switch_state_0, switch_state_1, switch_state_2;
-  RemoteController(int sw_pin0, int sw_pin1, int sw_pin2)
+  Switch(int sw_pin0, int sw_pin1, int sw_pin2)
   {
     switch_pin0 = sw_pin0;
     switch_pin1 = sw_pin1;
@@ -70,6 +70,7 @@ public:
 
   void setup_battery()
   {
+    analogReadResolution(12);
     pinMode(battery_pin, INPUT);
     pinMode(led_indicator_pin, OUTPUT);
     digitalWrite(led_indicator_pin, HIGH); // delete later
@@ -77,8 +78,8 @@ public:
 
   float read_battery_voltage()
   {
-    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 4.2V):
-    return analogRead(battery_pin) * (4.2 / 1023.0);
+    // Convert the analog reading (which goes from 0 - 4095) to a voltage (0 - 4.2V):
+    return analogRead(battery_pin) * (4.2 / 4095.0);
   }
 };
 
@@ -88,8 +89,8 @@ StaticJsonDocument<capacity_out> output_data;
 // Battery object
 Battery remoteBattery(ADC_BATTERY, GREEN_LED_BATTERY);
 // switches objects
-RemoteController heightSwitch(3, 4, 5);
-RemoteController massSwitch(0, 1, 2);
+Switch heightSwitch(3, 4, 5);
+Switch massSwitch(0, 1, 2);
 
 void setup()
 {
