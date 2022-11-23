de8e4a0fc7ce90f4889d9f7849e4cf94cc664b74
Kowalski <tomaszmarcin.kowalski@refinitiv.com>
2022-11-23 21:04:03 +0100

    Moved Adaptive valve class into header file

--- AdaptiveAirCushionMotorDrive/include/AdaptiveValve.h
+++ AdaptiveAirCushionMotorDrive/include/AdaptiveValve.h
@@ -8,49 +8,23 @@
 class AdaptiveValve
 {
 private:
-    TLE9201 motor_driver; // H-bridge motor driver object
-    int motor_channel; // motor driver channel
-    Adafruit_ADS1115 ads;
-    int ads_i2c_address;
-    uint8_t ads_analog_channel;
-    // valve position set point
-    float valve_pos_set; 
-    // angular positions limits
-    float ang_pos_valve_min;
-    float ang_pos_valve_max;
-    // voltage for adc calculations
-    const float u_min = 0.0;
-    const float u_max = 3.3;
-    // angular position variables [deg] - entire range of potentiometer 0 - 3600 deg
-    const float pot_pos_min = 0.0;
-    const float pot_pos_max = 3600.0;
-    // measurement gear nominal diameter
-    const float gear_diameter = 22.0;
-    // valve tolerance
-    const float valve_pos_tolerance = 10.0;
+    float _set_position;
 
 public:
-    // adc initialization status
-    bool adc_init_status;
-    // potentiometer output voltage [V]
-    float u;
-    // gear angular position [deg]
-    float ang_pos;
-    // valve displacement [mm]
-    float distance;
-
-    AdaptiveValve(int motor_channel,
-                  int ads_i2c_address, uint8_t ads_analog_channel,
-                  float ang_pos_valve_min, float ang_pos_valve_max, float valve_pos_set)
+    AdaptiveValve(float set_position = 1000)
     {
+        _set_position = set_position;
     }
-
-    void begin();
-    void set_position(float set_position);
-    void compute_position();
-    void print_set_position();
-    void print_actual_position();
-    void controller();
+    void set_position(float new_position)
+    {
+        Serial.print("Old position: ");
+        Serial.print(_set_position);
+        Serial.print(" [mm] \n");
+        _set_position = new_position;
+        Serial.print("New position: ");
+        Serial.print(_set_position);
+        Serial.print(" [mm] \n");
+    }
 };
 
 #endif
--- AdaptiveAirCushionMotorDrive/src/main.cpp
+++ AdaptiveAirCushionMotorDrive/src/main.cpp
@@ -1,137 +1,13 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <Adafruit_ADS1X15.h>
-#include <TLE9201.h>
 #include <LoRa.h>
 #include <cmath>
-
-class AdaptiveValve
-{
-private:
-  // motor driver channel
-  int motor_channel;
-  // H-bridge motor driver object
-  TLE9201 motor_driver;
-  // ADC and ADC channel variables
-  Adafruit_ADS1115 adc_ic;
-  int ads_i2c_address;
-  uint8_t ads_analog_channel;
-  // valve position set point
-  float valve_pos_set;
-  // angular positions limits
-  float ang_pos_valve_min;
-  float ang_pos_valve_max;
-  // voltage for adc calculations
-  const float u_min = 0.0;
-  const float u_max = 3.3;
-  // angular position variables [deg] - entire range of potentiometer 0 - 3600 deg
-  const float pot_pos_min = 0.0;
-  const float pot_pos_max = 3600.0;
-  // measurement gear nominal diameter
-  const float gear_diameter = 22.0;
-  // valve tolerance
-  const float valve_pos_tolerance = 10.0;
-
-public:
-  // adc initialization status
-  bool adc_init_status;
-  // potentiometer output voltage [V]
-  float u;
-  // gear angular position [deg]
-  float ang_pos;
-  // valve displacement [mm]
-  float distance;
-
-  AdaptiveValve(int motor_channel,
-                int ads_i2c_address, uint8_t ads_analog_channel,
-                float ang_pos_valve_min = 1120.0, float ang_pos_valve_max = 1490.0,
-                float valve_pos_set = 1140) // Domyslne wartosci nie przechodza
-  {
-    motor_channel = motor_channel;
-    ads_i2c_address = ads_i2c_address;
-    ads_analog_channel = ads_analog_channel;
-    ang_pos_valve_min = ang_pos_valve_min;
-    ang_pos_valve_max = ang_pos_valve_max;
-    valve_pos_set = valve_pos_set; //constrain(valve_pos_set, ang_pos_valve_min, ang_pos_valve_max);
-  }
-
-  // set valve position
-  void set_valve_position(float set_position)
-  {
-    valve_pos_set = set_position; // constrain(set_position, ang_pos_valve_min, ang_pos_valve_max);
-  }
-
-  // run this in void setup
-  void begin()
-  {
-    motor_driver.begin(motor_channel);
-    adc_init_status = adc_ic.begin(ads_i2c_address);
-  }
-  // Compute position function declaration
-  void compute_position()
-  {
-    Serial.println(adc_ic.readADC_SingleEnded(ads_analog_channel)); // replace ads_2 with class instance
-    // convert counts into voltage
-    //u = adc_ic.computeVolts(adc_ic.readADC_SingleEnded(ads_analog_channel)); // replace ads_2 with class instance
-    //Serial.println(u);
-    // calculate angular position;
-    //ang_pos = (u - u_min) * (pot_pos_max - pot_pos_min) / (u_max - u_min) + pot_pos_min;
-    //Serial.println(ang_pos);
-    // compute absolute distance
-    //distance = (ang_pos / 360.0) * 3.14159 * gear_diameter;
-    //Serial.println(distance);
-  }
-
-  void print_set_position()
-  {
-    Serial.print("Set position: ");
-    Serial.print(valve_pos_set);
-    Serial.print(" [deg], ");
-  }
-
-  // print position
-  void print_actual_position()
-  {
-    compute_position();
-    Serial.print("position: ");
-    Serial.print(ang_pos);
-    Serial.println(" [deg]");
-  }
-
-  void controller()
-  {
-    // update position on each call
-    compute_position();
-    // check if valve position is within tolerance
-    if (abs(valve_pos_set - ang_pos) >= valve_pos_tolerance)
-    {
-      // control motor direction
-      if (valve_pos_set >= ang_pos)
-      {
-        // turn motor on in forward direction
-        motor_driver.set_pwm_dir(1, 1);
-        Serial.println("Motor: ON, direction: UP");
-      }
-      else
-      {
-        // turn motor on in reverse direction
-        motor_driver.set_pwm_dir(1, 0);
-        Serial.println("Motor: ON, direction: DOWN");
-      }
-    }
-    else
-    {
-      // turn off the motor
-      motor_driver.set_pwm_dir(0, 0);
-      Serial.println("Valve in position : )");
-    }
-  }
-};
+#include <AdaptiveValve.h>
 
 // H-bridge object instance
 // TLE9201 h8(7);
 // Declare adaptive valve object
-AdaptiveValve ad_valve_1(7, 0b1001001, 3); // Nie przechodzi -> , 1120.0, 1490.0, 1000.0);
+AdaptiveValve ad_valve_1(1020.5);
 
 /* MAIN functions declaration */
 // void MAIN_print_hbridge_status(void);
@@ -142,23 +18,20 @@ void setup()
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Setup started!");
-  ad_valve_1.begin();
+  /*ad_valve_1.begin();
   // check if ADC for vavle 1 was initalized successfully
   if (!ad_valve_1.adc_init_status)
   {
     Serial.println("Failed to initialize ADS.");
     while (1);
-  }
-  Serial.println("Setup successfully completed!");
+  }*/
+  Serial.println("Setup successfully completedd!");
 }
 
 void loop()
 {
-  ad_valve_1.set_valve_position(1100);
-  ad_valve_1.print_set_position();
-  ad_valve_1.compute_position();
-  //ad_valve_1.print_actual_position();
-  //ad_valve_1.controller();
-  // wait for next loop iter
+  ad_valve_1.set_position(1250);
+  delay(2000);
+  ad_valve_1.set_position(985.7);
   delay(2000);
 }
