254d4602eb4935ae05b243325f6e6d76ca3d977f
Kowalski <tomaszmarcin.kowalski@refinitiv.com>
2022-11-24 12:16:22 +0100

    Updated header hile for adaptive valve classs

--- MotorDriver/include/AdaptiveValve.h
+++ MotorDriver/include/AdaptiveValve.h
@@ -1,4 +1,5 @@
-/*
+#ifndef __AdaptiveValve__
+#define __Adaptivevalve__
 
 #include <Arduino.h>
 #include <Adafruit_ADS1X15.h>
@@ -7,47 +8,30 @@
 class AdaptiveValve
 {
 private:
-  // motor driver channel
-  int motor_channel; 
-  // H-bridge motor driver object
-  TLE9201 motor_driver(int motor_channel);
-  // ADC and ADC channel variables
-  Adafruit_ADS1115 adc_ic;
-  uint8_t adc_channel;
-  // valve position set point
-  float valve_pos_set;
-  // angular positions limits
-  float ang_pos_valve_min;
-  float ang_pos_valve_max;
-  // voltage for adc calculations
-  const float u_min = 0.0;
-  const float u_max = 3.3;
-  // angular position variables [deg] - entire range of potentiometer 0 - 3600 deg
-  const float ang_pos_min = 0.0;
-  const float ang_pos_max = 3600.0;
-  // measurement gear nominal diameter
-  const float gear_diameter = 22.0;
-  // valve tolerance
-  const float valve_pos_tolerance = 10.0;
+    float _set_position;
 
 public:
-  // potentiometer output voltage [V]
-  float u;
-  // gear angular position [deg]
-  float ang_pos;
-  // valve displacement [mm]
-  float distance;
+    AdaptiveValve(float set_position = 1000)
+    {
+        _set_position = set_position;
+    }
 
-  AdaptiveValve(TLE9201 motor,  
-                Adafruit_ADS1115 ads1115, uint8_t channel,
-                float pos_min, float pos_max,
-                float set_position);
+    void print_position(){
+        Serial.print("Set position: ");
+        Serial.print(_set_position);
+        Serial.print(" [mm] \n");
+    }
 
-  void set_valve_position(float set_position);
-  void compute_position();
-  void controller();
-  void print_actual_position();
-  void print_set_position();
+    void set_position(float new_position)
+    {
+        Serial.print("Old position: ");
+        Serial.print(_set_position);
+        Serial.print(" [mm] \n");
+        _set_position = new_position;
+        Serial.print("New position: ");
+        Serial.print(_set_position);
+        Serial.print(" [mm] \n");
+    }
 };
 
-*/
+#endif
