57a5edaec1c697025321d0709c95642c4065b880
ramif <ramif@wp.pl>
2022-11-26 23:22:05 +0100

    motor drive controller tested sucessfully

--- MotorDriver/src/main.cpp
+++ MotorDriver/src/main.cpp
@@ -26,18 +26,30 @@ void setup()
 
 void loop()
 {
+  // change valve position
+  ad_valve_1.set_position(10);
+  ad_valve_1.controller();
+  ad_valve_1.print_position();
+  // tested -- motor drive is running in one direction if measured
+  // position is lower than set_position with _displacement_tolerance
+  // stops when position is within tolerance range and is running again but
+  // in opposite direction if set_position is higher than set_position
+  // with _displacement_tolerance
+
   // open valve
-  ad_valve_1.set_position(0);
-  while (ad_valve_1.controller())
-  {
-    ad_valve_1.print_position();
-    delay(500);
-  }
+  // ad_valve_1.set_position(0);
+  //ad_valve_1.print_position();
+  //delay(10000);
+  // while (ad_valve_1.controller())
+  // {
+  //  ad_valve_1.print_position();
+  //  delay(2000);
+  //}
   // close valve
-  ad_valve_1.set_position(70);
-  while (ad_valve_1.controller())
-  {
-    ad_valve_1.print_position();
-    delay(500);
-  }
+  // ad_valve_1.set_position(50);
+  // while (ad_valve_1.controller())
+  //{
+  //  ad_valve_1.print_position();
+  //  delay(2000);
+  // }
 }
