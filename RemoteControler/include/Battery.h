148c210e01a5c44d42c2a05c07b3e6a670bed281
Kowalski <tomaszmarcin.kowalski@refinitiv.com>
2022-11-29 18:30:15 +0100

    Moved Battery and Switch class definitions into separate files

--- RemoteControler/include/Battery.h
+++ RemoteControler/include/Battery.h
@@ -10,28 +10,9 @@ private:
   int _led_indicator_pin;
 
 public:
-  Battery(int bat_pin, int led_pin)
-  {
-    _battery_pin = bat_pin;
-    _led_indicator_pin = led_pin;
-    analogReadResolution(12);
-    pinMode(_battery_pin, INPUT);
-    pinMode(_led_indicator_pin, OUTPUT);
-    digitalWrite(_led_indicator_pin, HIGH);
-  };
-
-  float read_battery_voltage()
-  {
-    // Convert the analog reading (which goes from 0 - 4095) to a voltage (0 - 4.2V):
-    return (analogRead(_battery_pin) * (3.223 / 4095.0)) * (4.2 / 3.223);
-  }
-  bool low_battery_warning(float battery_voltage)
-  {
-    if (battery_voltage >= 3.15)
-      return false;
-    else
-      return true;
-  }
+  Battery(int bat_pin, int led_pin);
+  float read_battery_voltage();
+  bool low_battery_warning(float battery_voltage);
 };
 
 #endif
--- RemoteControler/include/Switch.h
+++ RemoteControler/include/Switch.h
@@ -10,43 +10,8 @@ private:
 
 public:
   bool switch_state_0, switch_state_1, switch_state_2;
-  Switch(int sw_pin0, int sw_pin1, int sw_pin2)
-  {
-    _switch_pin0 = sw_pin0;
-    _switch_pin1 = sw_pin1;
-    _switch_pin2 = sw_pin2;
-    pinMode(_switch_pin0, INPUT_PULLUP);
-    pinMode(_switch_pin1, INPUT_PULLUP);
-    pinMode(_switch_pin2, INPUT_PULLUP);
-  }
-
-  int read_swich_state()
-  {
-    // read state of switches (NC logic)
-    switch_state_0 = digitalRead(_switch_pin0);
-    switch_state_1 = digitalRead(_switch_pin1);
-    switch_state_2 = digitalRead(_switch_pin2);
-    // code state 0 if both switches are shorted
-    if ((switch_state_0 == true) & (switch_state_2 == false))
-    {
-      return 0;
-    }
-    // code state 1 if switch0 is shorted and switch1 is open
-    else if ((switch_state_0 == false) & (switch_state_2 == true))
-    {
-      return 2;
-    }
-    // code state 1 if both switches are open
-    else if ((switch_state_0 & switch_state_2) == false)
-    {
-      return 1;
-    }
-    // return error for uknown states
-    else
-    {
-      return -1;
-    }
-  }
+  Switch(int sw_pin0, int sw_pin1, int sw_pin2);
+  int read_swich_state();
 };
 
 #endif
