0633d2042a89502c96bc32438bdad15e6b9d5565
ramif <ramif@wp.pl>
2022-12-12 22:16:19 +0100

    LoRa setup added

--- MotorDriver/src/main.cpp
+++ MotorDriver/src/main.cpp
@@ -2,17 +2,21 @@
 #include <arduino-timer.h>
 #include <ArduinoJson.h>
 #include <LoRa.h>
+#include <loraHelperFunctions.h>
 #include <AdaptiveValve.h>
 
 // create a timer with default settings
 auto timer = timer_create_default();
 // Declare adaptive valve object
-AdaptiveValve ad_valve_1(0b1001001, 3, 7, 0.0, 0.0, 800.0);
+AdaptiveValve ad_valve_1(0b1001001, 3, 7, 390.0, 380.0, 400.0);
 AdaptiveValve ad_valve_2(0b1001001, 2, 6);
+// LoRa Frequency
+const long frequency = 868E6;
 
 bool print_position(void *)
 {
-  ad_valve_1.print_position();
+  // ad_valve_1.get_position();
+  // ad_valve_1.print_position();
   return true;
 }
 
@@ -20,8 +24,19 @@ void setup()
 {
   // start serial comunication
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("Setup started!");
+  if (!LoRa.begin(frequency))
+  {
+    Serial.println("Starting LoRa failed!");
+    while (true);
+  }
+
+  LoRa.onReceive(onReceive);
+  LoRa.onTxDone(onTxDone);
+  LoRa_rxMode();
+
   // check if ADC for vavle 1 was initalized successfully
   Serial.println(ad_valve_1.begin());
   Serial.println(ad_valve_2.begin());
@@ -31,25 +46,25 @@ void setup()
 
 void loop()
 {
-  timer.tick();
-  /*
-  ad_valve_1.set_position(ad_valve_1.decode_position(2));
+  delay(1000);
+  // timer.tick();
+  /*  ad_valve_1.set_position(ad_valve_1.decode_position(2));
   delay(2000);
   ad_valve_1.set_position(ad_valve_1.decode_position(6));
   delay(2000);
   */
- 
-  /*
+
   // open valve
-  ad_valve_1.set_position(0);
-  while (ad_valve_1.controller());
-  Serial.println("Valve in position!");
-  delay(2000);
+  // float pos1 = ad_valve_1.decode_position(0), pos2 = ad_valve_1.decode_position(8);
+  // Serial.println(pos1);
+  // ad_valve_1.set_position(ad_valve_1.decode_position(0));
+  // while (ad_valve_1.controller());
+  // Serial.println("Valve in position!");
+  // delay(2000);
   // close valve
-  ad_valve_1.set_position(70);
-  while (ad_valve_1.controller());
-  Serial.println("Valve in position!");
-  delay(2000);
-  */
-  
+  // Serial.println(pos2);
+  // ad_valve_1.set_position(ad_valve_1.decode_position(8));
+  // while (ad_valve_1.controller());
+  // Serial.println("Valve in position!");
+  // delay(2000);
 }
